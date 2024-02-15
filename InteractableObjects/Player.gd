43e08ad3e2c9957233348d4499bd073d25e3c83c
Matthew Nelson <32201016+RepentMF@users.noreply.github.com>
2024-02-15 18:48:30 -0500

    made swinging mechanics woo!

--- InteractableObjects/Player.gd
+++ InteractableObjects/Player.gd
@@ -4,10 +4,12 @@ var MinSpeed = 100
 var MaxSpeed = 400
 const Up = Vector2(0, -1)
 
+var airTimeCount = 0
 var climbXValue = 0
 var climbYValue = 0
 var groundDirection = 0
 var gravity = 300
+var hasReleasedRope = false
 var isClimbingLedge = false
 var isCrawlingLedge = false
 var isFalling = false
@@ -16,6 +18,7 @@ var isRunning = false
 var isSwinging = false
 var jumpVelocity = 100
 var speed = 100
+var swingSpeed = 0
 var swingRope = null
 var wasJumping = false
 
@@ -24,14 +27,31 @@ var wasJumping = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	print(isSwinging, swingRope)
 	pass
 
 func _physics_process(delta):
 	#Handle death conditions
 	#if !isDead || !hasReset:
+		if hasReleasedRope:
+			airTimeCount += 1
+			if airTimeCount > 30:
+				airTimeCount = 0
+				$CollisionShape2D.disabled = false
+				isSwinging = false
+				hasReleasedRope = false
+		# Handle swinging on a rope
 		if isSwinging && swingRope != null:
 			global_position = global_position.move_toward(Vector2(swingRope.global_position.x, swingRope.global_position.y), 10)
 			$CollisionShape2D.disabled = true
+			if Input.is_action_just_pressed("ui_right"):
+				swingRope.apply_impulse(Vector2(10, 0))
+			elif Input.is_action_just_pressed("ui_left"):
+				swingRope.apply_impulse(Vector2(-10, 0))
+			elif Input.is_action_just_pressed("ui_accept"):
+				velocity = swingRope.linear_velocity
+				swingRope = null
+				hasReleasedRope = true
 		# Handle climbing a ledge
 		elif isGrabbingLedge:
 			velocity.x = 0
@@ -50,6 +70,7 @@ func _physics_process(delta):
 			groundDirection = sign(velocity.x)
 			wasJumping = true
 		else:
+			isSwinging = false
 			if isFalling:
 				isFalling = false
 			elif wasJumping:
@@ -83,12 +104,18 @@ func _physics_process(delta):
 		
 		move_and_slide()
 		
+		# Apply initial force to rope
 		for n in get_slide_collision_count():
 			var i = get_slide_collision(n)
 			if i.get_collider() is RigidBody2D && i.get_collider().name.contains("RopeLinkage"):
-				i.get_collider().apply_impulse(Vector2(-i.get_normal().x, 0) * 0.25 * get_real_velocity().x)
-				isSwinging = true
-				#get_tree().paused = true
+				i.get_collider().apply_impulse(Vector2(0.2 * swingSpeed, 0))
+				if Input.is_action_pressed("ui_select"):
+					isSwinging = true
+					swingRope = i.get_collider()
+		if isSwinging && swingRope != null:
+			swingSpeed = swingRope.linear_velocity.x
+		else:
+			swingSpeed = velocity.x
 		pass
 
 func ascend_ledge():
@@ -105,7 +132,7 @@ func crawl_ledge():
 		isCrawlingLedge = false
 		speed = MinSpeed
 
-func _on_grab_area_2d_1_body_entered(body):
+func use_hands(body):
 	if body.name.contains("LedgeGrab"):
 		if velocity.x != MaxSpeed:
 			climbYValue = body.global_position.y + 1
@@ -116,21 +143,12 @@ func _on_grab_area_2d_1_body_entered(body):
 			isFalling = true
 	elif body.name.contains("Floor"):
 		isFalling = true
-	elif body.name.contains("RopeLinkage"):
-	#	isSwinging = true
-		swingRope = body
+	
+func _on_grab_area_2d_1_body_entered(body):
+	use_hands(body)
 	pass # Replace with function body.
 
 
 func _on_grab_area_2d_2_body_entered(body):
-	if body.name.contains("LedgeGrab"):
-		if velocity.x != MaxSpeed:
-			climbYValue = body.global_position.y + 1
-			climbXValue = body.global_position.x
-			isGrabbingLedge = true
-			isFalling = false
-		else:
-			isFalling = true
-	elif body.name.contains("Floor"):
-		isFalling = true
+	use_hands(body)
 	pass # Replace with function body.
