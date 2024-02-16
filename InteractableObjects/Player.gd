253383c1ada2f7ff629ae7142bcbe50a1a27ab18
Matthew Nelson <32201016+RepentMF@users.noreply.github.com>
2024-02-15 23:50:31 -0500

    made rocket jumping woo!

--- InteractableObjects/Player.gd
+++ InteractableObjects/Player.gd
@@ -7,6 +7,7 @@ const Up = Vector2(0, -1)
 var airTimeCount = 0
 var climbXValue = 0
 var climbYValue = 0
+var fallDistanceCount = 0
 var groundDirection = 0
 var gravity = 300
 var hasReleasedRope = false
@@ -27,12 +28,12 @@ var wasJumping = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(isSwinging, swingRope)
 	pass
 
 func _physics_process(delta):
 	#Handle death conditions
 	#if !isDead || !hasReset:
+		# Handle releasing a rope
 		if hasReleasedRope:
 			airTimeCount += 1
 			if airTimeCount > 30:
@@ -45,10 +46,10 @@ func _physics_process(delta):
 			global_position = global_position.move_toward(Vector2(swingRope.global_position.x, swingRope.global_position.y), 10)
 			$CollisionShape2D.disabled = true
 			if Input.is_action_just_pressed("ui_right"):
-				swingRope.apply_impulse(Vector2(10, 0))
+				swingRope.apply_impulse(Vector2(15, 0))
 			elif Input.is_action_just_pressed("ui_left"):
-				swingRope.apply_impulse(Vector2(-10, 0))
-			elif Input.is_action_just_pressed("ui_accept"):
+				swingRope.apply_impulse(Vector2(-15, 0))
+			elif Input.is_action_just_released("ui_cancel"):
 				velocity = swingRope.linear_velocity
 				swingRope = null
 				hasReleasedRope = true
@@ -69,7 +70,31 @@ func _physics_process(delta):
 			isRunning = false
 			groundDirection = sign(velocity.x)
 			wasJumping = true
+			if velocity.y > 400:
+				isFalling = true
+				fallDistanceCount += 1
+				if fallDistanceCount > 150:
+					print("Player is kill")
+			# Handle rocket jumps
+			if Input.is_action_just_pressed("ui_accept"):
+				if Input.is_action_pressed("ui_right"):
+					velocity.x = 250
+				elif Input.is_action_pressed("ui_left"):
+					velocity.x = -250
+				else:
+					velocity.x = 0
+				if Input.is_action_pressed("ui_up"):
+					velocity.y = -250
+				elif Input.is_action_pressed("ui_down"):
+					velocity.y = 250
+				elif !Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
+					velocity.y = -250
+				print(velocity)
+				if abs(velocity.x) == abs(velocity.y):
+					velocity = velocity * 4 / 3
+		# Handle grounded movement
 		else:
+			fallDistanceCount = 0
 			isSwinging = false
 			if isFalling:
 				isFalling = false
@@ -78,9 +103,9 @@ func _physics_process(delta):
 					speed = MinSpeed
 					wasJumping = false
 			# Enable running toggle
-			if Input.is_action_pressed("ui_cancel"):
+			if Input.is_action_pressed("ui_select"):
 				isRunning = true
-			elif Input.is_action_just_released("ui_cancel"):
+			elif Input.is_action_just_released("ui_select"):
 				isRunning = false
 			# Handle jumping
 			if Input.is_action_just_pressed("ui_accept"):
@@ -108,8 +133,8 @@ func _physics_process(delta):
 		for n in get_slide_collision_count():
 			var i = get_slide_collision(n)
 			if i.get_collider() is RigidBody2D && i.get_collider().name.contains("RopeLinkage"):
-				i.get_collider().apply_impulse(Vector2(0.2 * swingSpeed, 0))
-				if Input.is_action_pressed("ui_select"):
+				i.get_collider().apply_impulse(Vector2(0.25 * swingSpeed, 1))
+				if Input.is_action_pressed("ui_cancel"):
 					isSwinging = true
 					swingRope = i.get_collider()
 		if isSwinging && swingRope != null:
@@ -140,8 +165,10 @@ func use_hands(body):
 			isGrabbingLedge = true
 			isFalling = false
 		elif velocity.x == MaxSpeed:
+			velocity.y = 0
 			isFalling = true
 	elif body.name.contains("Floor"):
+		velocity.y = 0
 		isFalling = true
 	
 func _on_grab_area_2d_1_body_entered(body):
