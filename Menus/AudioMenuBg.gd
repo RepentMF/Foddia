071854d812090a4944934a3a2c5c2c789b0aead9
Matthew Nelson <32201016+RepentMF@users.noreply.github.com>
2024-08-23 17:22:57 -0400

    added an audio menu background and stylized

---
+++ Menus/AudioMenuBg.gd
@@ -0,0 +1,32 @@
+extends Sprite2D
+
+var random
+
+var bounce = 0
+var count = 0
+var move = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	random = RandomNumberGenerator.new().randi_range(0, 100)
+	if random >= 91 && bounce == 0:
+		move = 1
+		bounce = 2
+		count = 0
+		position = Vector2(47.75, 36.75)
+	
+	if count % 2:
+		position.y += move
+	else:
+		position.y -= move
+	
+	if bounce > 0:
+		bounce -= 1
+	else:
+		move = 2
+	count += 1
+	pass
