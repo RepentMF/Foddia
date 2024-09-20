2d4230a0715fcff6d1537e5c3156911c24f709c9
RepentMF <32201016+RepentMF@users.noreply.github.com>
2024-09-20 17:13:22 -0400

    added first NPC

--- Menus/SwingingObject.gd
+++ Menus/SwingingObject.gd
@@ -1,26 +1,37 @@
 extends Sprite2D
 
 var user_prefs: UserPreferences
+var item_string
 
 var rotation_diff
+var x_diff
 var swing_right = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rotation_diff = -0.1
+	user_prefs = UserPreferences.load_or_create()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print(rotation)
-	
-	if swing_right:
-		rotation_diff = -0.1
+func _physics_process(delta):
+	item_string = get_parent()
+	if user_prefs.screenshake_bool_check:
+		item_string.play("swinging")
+		if swing_right:
+			rotation_diff = -0.0349066
+			x_diff = .5
+		else:
+			rotation_diff = 0.0349066
+			x_diff = -.5
+		
+		rotation += rotation_diff
+		position.x += x_diff
+		
+		if abs(rotation) >= .52:
+			if swing_right:
+				swing_right = false
+			elif !swing_right:
+				swing_right = true
 	else:
-		rotation_diff = 0.1
-	
-	rotation += rotation_diff
-	
-	if abs(rotation) >= 0.53:
-		swing_right = swing_right * -1
+		item_string.play("idle")
 	pass
